86766bd42348c97aca8149629e5e967f3a91b24e
rastalord <40312870+rastalord@users.noreply.github.com>
2021-08-06 11:36:44 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,36 @@
+import urllib.request
+import urllib.parse
+from bs4 import BeautifulSoup
+import re
+
+url = 'https://sec.report/Document/0001193125-21-072571'
+
+urls = ['https://sec.report/Document/0001193125-21-072571', 'https://sec.report/Document/0001193125-21-031547/',
+        'https://sec.report/Document/0001104659-21-037813/', 'https://sec.report/Document/0001193125-21-024746/',
+        'https://sec.report/Document/0001193125-21-056708/']
+fnames = ['text1.html', 'text2.html', 'text3.html', 'text4.html', 'text5.html']
+
+for i in range(len(urls)):
+    req = urllib.request.Request(urls[i], headers={'User-Agent': 'Mozilla/5.0'})
+    webpage = urllib.request.urlopen(req).read()
+
+    f = open(fnames[i], 'wb')
+    f.write(webpage)
+    f.close()
+
+with open("D:/Ivan/CPP_1/test/text1.html") as fp:
+    soup = BeautifulSoup(fp, 'html.parser')
+
+answ = []
+#num = re.findall(r'(?:\d+\.)?\d+,\d+', str(td))
+data = soup.find_all('td')
+for td in data:
+    num = re.findall(r'\d+,\d+,\d+', str(td))
+    answ.append(num)
+    num = re.findall(r'\d+\.+\d+', str(td))
+    answ.append(num)
+    num = re.findall(r'\d+-\d+', str(td))
+    answ.append(num)
+answ = list(filter(None, answ))
+print(answ)
+
